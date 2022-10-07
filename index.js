051d4a5f0bdcc44cd26f33dfd2d1007ca223e259
Pelin Karakoca <karakocapelin@gmail.com>
2022-10-07 14:23:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+//For first test: Multipliying 
+const num1 = 22;
+const num2 = 40;
+const multiply = num1 + num2;
+
+//Second test: Random number generator
+const random = Math.round(Math.random() * 10);
+
+//Third test: Mod
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+//Fourth test: Highest number
+const max = Math.max(1, 20, 19, 18, 3, 5, 6, 7);
